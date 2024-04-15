a2d7d933f9bfcdbbcb7e47bf29fafaac4f447ca4
Matheus Fritzke <matheus.fritzke@clinicorp.com>
2024-04-15 16:34:49 -0300

    init project ignite-call

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { globalCss } from '@ignite-ui/react'
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  body: {
+    backgroundColor: '$gray900',
+    color: '$gray100',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+
+  '#__next': {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+})
